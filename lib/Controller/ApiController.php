35de80f1f01e6d1bb48d5eeb38a409d51fdf0cf2
filzmann <simonbeyer79@gmail.com>
2026-07-04 15:57:31 +0200

    Use LocalBase API responder

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -10,6 +10,7 @@ use OCA\BrStunden\Service\BrStundenLogger;
 use OCA\BrStunden\Service\HoursService;
 use OCA\BrStunden\Service\PayrollPdfService;
 use OCA\BrStunden\Service\ReminderService;
+use OCA\LocalBase\Controller\ApiResponder;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
@@ -25,25 +26,36 @@ class ApiController extends Controller {
         private ReminderService $reminders,
         private PayrollPdfService $payrollPdf,
         private BrMemberService $members,
-        private BrStundenLogger $logger
+        private BrStundenLogger $logger,
+        private ApiResponder $responder
     ) {
         parent::__construct(Application::APP_ID, $request);
     }
 
     #[NoAdminRequired]
     public function state(): DataResponse {
-        return $this->respond(fn(): array => $this->hours->state(), 'state');
+        return $this->responder->respond(
+            fn(): array => $this->hours->state(),
+            [$this->logger, 'error'],
+            'state'
+        );
     }
 
     #[NoAdminRequired]
     public function year(int $year): DataResponse {
-        return $this->respond(fn(): array => $this->hours->yearOverview($year), 'year', ['year' => $year]);
+        return $this->responder->respond(
+            fn(): array => $this->hours->yearOverview($year),
+            [$this->logger, 'error'],
+            'year',
+            ['year' => $year]
+        );
     }
 
     #[NoAdminRequired]
     public function saveEntry(int $year, int $month, string $hours = '', string $fobiHours = '', string $note = ''): DataResponse {
-        return $this->respond(
+        return $this->responder->respond(
             fn(): array => $this->hours->saveEntry($year, $month, $hours, $fobiHours, $note),
+            [$this->logger, 'error'],
             'save_entry',
             ['year' => $year, 'month' => $month]
         );
@@ -51,8 +63,9 @@ class ApiController extends Controller {
 
     #[NoAdminRequired]
     public function deleteEntry(int $year, int $month): DataResponse {
-        return $this->respond(
+        return $this->responder->respond(
             fn(): array => $this->hours->deleteEntry($year, $month),
+            [$this->logger, 'error'],
             'delete_entry',
             ['year' => $year, 'month' => $month]
         );
@@ -80,27 +93,10 @@ class ApiController extends Controller {
 
     #[NoAdminRequired]
     public function reminderPreview(): DataResponse {
-        return $this->respond(function(): array {
+        return $this->responder->respond(function(): array {
             $this->members->assertCurrentUserIsMember();
 
             return $this->reminders->previewMonthlyReminders();
-        }, 'reminder_preview');
-    }
-
-    private function respond(callable $callback, string $action, array $context = []): DataResponse {
-        try {
-            return new DataResponse($callback());
-        } catch (\InvalidArgumentException $e) {
-            return new DataResponse(['ok' => false, 'message' => $e->getMessage()], Http::STATUS_BAD_REQUEST);
-        } catch (\DomainException $e) {
-            return new DataResponse(['ok' => false, 'message' => $e->getMessage()], Http::STATUS_FORBIDDEN);
-        } catch (\Throwable $e) {
-            $this->logger->error($action, $e, $context);
-
-            return new DataResponse([
-                'ok' => false,
-                'message' => 'Die Aktion konnte nicht ausgefuehrt werden. Details stehen im Nextcloud-Log.',
-            ], Http::STATUS_INTERNAL_SERVER_ERROR);
-        }
+        }, [$this->logger, 'error'], 'reminder_preview');
     }
 }
